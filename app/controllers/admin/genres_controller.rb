ef64df07fef7e89ee61aa3d95339efba548e0b81
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2022-11-28 16:20:44 +0000

    [Fin] 管理者ログイン画面及び商品一覧画面のレイアウト完成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,44 @@
 class Admin::GenresController < ApplicationController
+  before_action :set_genre, only: [:edit, :update, :destroy]
+
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to categories_path
+    else
+      @genres = Genre.all
+      render 'index'
   end
 
   def edit
   end
+
+   def update
+    if @genre.update(genre_params)
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @genre.destroy
+    redirect_to categories_path
+  end
+
+  private
+  
+  def set_genre
+      @genre = Genre.find(params[:id])
+  end
+
+  def genre_params
+      params.require(:category).permit(:name)
+  end
+
 end
