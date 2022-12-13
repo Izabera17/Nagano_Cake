c561c5d165cb18ba294311092ca64fa518a3f7b8
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2022-12-13 11:20:10 +0000

    [Add] 注文ステータス及び製作ステータス機能の追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,21 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   
   def update
-    
+		@order_detail = OrderDetail.find(params[:id]) 
+		@order = @order_detail.order #
+		@order_detail.update(order_detail_params) #　製作ステータスの更新
+
+		if @order_detail.crafting_status == "production" #製作ステータスが「製作作中」なら入る
+			@order.update(order_status: 2) #注文ステータスを「製作中」　に更新
+		elsif @order.order_details.count ==  @order.order_details.where(crafting_status: "complete").count
+			@order.update(order_status: 3) #注文ステータスを「発送準備中]に更新
+		end
+		redirect_to admin_order_path(@order_detail.order) #注文詳細に遷移
+	end
+
+	private
+
+	  def order_detail_params
+      params.require(:order_detail).permit(:crafting_status)
+    end
   end
-end
